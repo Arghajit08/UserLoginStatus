af925477df1eb5ca9aafa9dd76711a7fb049a9df
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-03-07 16:47:00 +0530

    Update managers.py

--- login/managers.py
+++ login/managers.py
@@ -6,19 +6,19 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, email, password, phoneno):
+    def create_user(self, email, password):
         """
         Create and save a User with the given email and password.
         """
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(email=email, password=password, phoneno=phoneno)
+        user = self.model(email=email, password=password)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, phoneno):
+    def create_superuser(self, email, password):
         """
         Create and save a SuperUser with the given email and password.
         """
@@ -30,4 +30,4 @@ class CustomUserManager(BaseUserManager):
         #     raise ValueError(_('Superuser must have is_staff=True.'))
         # if extra_fields.get('is_superuser') is not True:
         #     raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, phoneno)
+        return self.create_user(email, password)
